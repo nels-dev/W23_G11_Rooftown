95fba99044469b4032642e8f3424d484156b5a44
aldo <aldoque0304@gmail.com>
2023-03-28 17:37:39 -0700

    RM18 - Post Detail Page - Create post detail page - Create onclicklistener from view (grid & list) to product detail page - Populate different information based on post type

--- app/src/main/java/csis3175/w23/g11/rooftown/posts/ui/view/PostDetailFragment.java
+++ app/src/main/java/csis3175/w23/g11/rooftown/posts/ui/view/PostDetailFragment.java
@@ -15,9 +15,15 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.w3c.dom.Text;
+
+import java.util.UUID;
+
 import csis3175.w23.g11.rooftown.R;
 import csis3175.w23.g11.rooftown.posts.data.model.Post;
+import csis3175.w23.g11.rooftown.posts.data.model.PostType;
 import csis3175.w23.g11.rooftown.posts.ui.viewmodel.PostViewModel;
+import csis3175.w23.g11.rooftown.util.ImageFileHelper;
 
 public class PostDetailFragment extends Fragment {
     private Post post;
@@ -25,8 +31,12 @@ public class PostDetailFragment extends Fragment {
     public static final String ARG_POST_ID = "post_id";
     private static final String TAG = "POSTS";
     private ImageView imgViewPosting;
-    private TextView txtViewPostingLocation;
-    private TextView txtViewPostingDescription;
+    private TextView txtViewPosting1stLine;
+    private TextView txtViewPosting2ndLine;
+    private TextView txtViewPosting3rdLine;
+    private ImageView imgViewPostDetailAvator;
+    private TextView txtViewPostDetailInitiatorName;
+    private TextView txtViewPostDetailInitiatorDescription;
 
     public static PostDetailFragment newInstance(){
         return new PostDetailFragment();
@@ -35,20 +45,49 @@ public class PostDetailFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_posting, container,false);
+        return inflater.inflate(R.layout.fragment_post_detail, container,false);
     }
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstancedState){
         super.onViewCreated(view, savedInstancedState);
         viewModel = new ViewModelProvider(getActivity()).get(PostViewModel.class);
-        ImageView imgViewPosting = view.findViewById(R.id.imgViewPosting);
-        TextView txtViewPostingLocation = view.findViewById(R.id.txtViewPostingLocation);
-        TextView txtViewPostingDescription = view.findViewById(R.id.txtViewPostingDescription);
+        imgViewPosting = view.findViewById(R.id.imgViewPosting);
+        txtViewPosting1stLine = view.findViewById(R.id.txtViewPosting1stLine);
+        txtViewPosting2ndLine = view.findViewById(R.id.txtViewPosting2ndLine);
+        txtViewPosting3rdLine = view.findViewById(R.id.txtViewPosting3rdLine);
+        imgViewPostDetailAvator = view.findViewById(R.id.imgViewPostDetailAvator);
+        txtViewPostDetailInitiatorName = view.findViewById(R.id.txtViewPostDetailInitiatorName);
+        txtViewPostDetailInitiatorDescription = view.findViewById(R.id.txtViewPostDetailInitiatorDescription);
+
+
         if (null == getArguments() || null == this.getArguments().getString(ARG_POST_ID)){
             Log.e(TAG, "No post is passed to this fragment. Expected " + ARG_POST_ID);
             return;
         }
-//        UUID postId = UUID.fromString(this.getArguments().getString(ARG_POST_ID));
+
+        Post post = viewModel.getPost(UUID.fromString(this.getArguments().getString(ARG_POST_ID)));
+
+        if (post.getPostType() == PostType.ROOM){
+            ImageFileHelper.readImage(view.getContext(), post.getRoomImage(), (bitmap) -> imgViewPosting.setImageBitmap(bitmap));
+            txtViewPosting1stLine.setText(post.getLocation());
+            txtViewPosting2ndLine.setText(post.getCity());
+            txtViewPosting3rdLine.setText(post.getRoomDescription());
+
+            ImageFileHelper.readImage(view.getContext(), post.getInitiatorImage(), (bitmap) -> imgViewPostDetailAvator.setImageBitmap(bitmap));
+            txtViewPostDetailInitiatorName.setText(post.getInitiatorName());
+            txtViewPostDetailInitiatorDescription.setText(post.getInitiatorDescription());
+        } else if (post.getPostType() == PostType.PERSON){
+            ImageFileHelper.readImage(view.getContext(), post.getInitiatorImage(), (bitmap) -> imgViewPosting.setImageBitmap(bitmap));
+            txtViewPosting1stLine.setText(post.getInitiatorName());
+            txtViewPosting2ndLine.setText(post.getInitiatorGender());
+            txtViewPosting3rdLine.setText(post.getInitiatorDescription());
+
+            TextView txtViewPostBy = view.findViewById(R.id.txtViewPostBy);
+            txtViewPostBy.setVisibility(View.INVISIBLE);
+            imgViewPostDetailAvator.setVisibility(View.INVISIBLE);
+            txtViewPostDetailInitiatorName.setVisibility(View.INVISIBLE);
+            txtViewPostDetailInitiatorDescription.setVisibility(View.INVISIBLE);
+        }
     }
 
     @Override
--- app/src/main/java/csis3175/w23/g11/rooftown/posts/ui/viewmodel/PostViewModel.java
+++ app/src/main/java/csis3175/w23/g11/rooftown/posts/ui/viewmodel/PostViewModel.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel;
 import com.google.firebase.firestore.ListenerRegistration;
 
 import java.util.List;
+import java.util.UUID;
 
 import csis3175.w23.g11.rooftown.posts.data.model.Post;
 import csis3175.w23.g11.rooftown.posts.data.repository.PostRepository;
@@ -22,6 +23,15 @@ public class PostViewModel extends ViewModel {
     }
 
     public LiveData<List<Post>> getAllPosts() { return posts; }
+
+    public Post getPost(UUID postId){
+        for (Post post : posts.getValue()){
+            if (post.getPostId().equals(postId)){
+                return post;
+            }
+        }
+        return null;
+    }
 
     public void loadData() {
         if (allPostsRegistration != null) {
